0f60306426e63dfdd0ba234fd654484d92373f08
aaltamirano1 <angel.g.altam@gmail.com>
2018-05-01 00:09:34 -0400

    Added student search functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 $(".page").append("<div class='pagination'></div>");
 $('.student-item').hide();
+$(".page-header").append(`
+	<div class='student-search'>
+		<input type="text" name="search">
+		<button>Search</button>
+	</div>
+`);
 
 var allStudents = [];
 $('li').each(function(i, element){
@@ -42,14 +48,58 @@ function display(){
 	});
 }
 
+// listen for changes in selected link and update display.
+function paginatnListener(){
+	$('.pagination li').click(function(event){
+		$('.student-item').hide();
+		selected = $(this).attr('id')-1;
+		display();
+	});
+}
+
 groupListItems(allStudents);
 buildPagination();
-var selected = 0; // initial selected pagination linki s the first group.
+var selected = 0; // initial selected pagination link is the first group.
 display();
 
-// listen for changes in selected link and update display.
-$('.pagination li').click(function(event){
-	$('.student-item').hide();
-	selected = $(this).attr('id')-1;
-	display();
+paginatnListener();
+
+// listen for student search
+$('.student-search button').click(function(){
+	var search = $('.student-search input').val();
+	// clear error messages if any.
+	$('#error').remove();
+	// clear pagination links in case no results found.
+	$(".pagination").empty();
+
+	// if search cleared display all students.
+	if (search === ''){
+		groupListItems(allStudents);
+		buildPagination();
+		selected = 0; 
+		display();
+
+	// else filter all students by name and email matches.
+	}else{
+		var matches = allStudents.filter(function(student){
+			var name = student.getElementsByTagName('h3')[0].textContent;
+			var email = student.getElementsByClassName("email")[0].textContent;
+			return name.indexOf(search)>=0||email.indexOf(search)>=0;
+		});
+
+		// if no matches found hide all student list items and show error msg.
+		if (matches.length==0){
+			$('.student-item').hide();
+			$('.student-list').append("<p id='error'>No students found.</p>");
+
+		// else display matches in groups of 10 students and build pagination accordingly. 
+		// also listen for clicks on pagination links.
+		}else{
+			groupListItems(matches);
+			buildPagination();
+			selected = 0; 
+			display();
+			paginatnListener();
+		}		
+	}
 });
