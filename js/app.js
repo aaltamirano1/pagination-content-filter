0a06781807518447e2d7ae2b2fbdb6c96845ffa4
Angel Altamirano <angel.g.altam@gmail.com>
2019-01-11 18:56:38 -0500

    refactor js to avoid global vars

--- js/app.js
+++ js/app.js
@@ -1,109 +1,93 @@
-$(".page").append("<div class='pagination'></div>");
-$('.student-item').hide();
-$(".page-header").append(`
-	<div class='student-search'>
-		<input type="text" name="search">
-		<button>Search</button>
-	</div>
-`);
+const showSelected=(selected)=>{
+	let minShown = (selected-1)*10;
+	let maxShown = minShown+10;
 
-var allStudents = [];
-$('li').each(function(i, element){
-	allStudents.push(element);
-});
-
-// a group of list items that will display on each "page".
-var liGroup = [];
-// an arr of groups.
-var liGroups = [];
-
-function groupListItems(listItems){
-	liGroups = [];
-	listItems.forEach(function(student, i){
-		// construct groups.
-		liGroup.push(student);
-		if((i+1)%10==0){
-			//  construct liGroups arr and clear group arr for next grouping.
-			liGroups.push(liGroup);
-			liGroup = [];
+	$('.visible').each(function(i, element){
+		if (i>=minShown){
+			$(element).show();
+		}
+		if (i>=maxShown){
+			$(element).hide();
 		}
 	});
-	// contruct last group out of the left-over list items.
-	liGroups.push(liGroup);
-	liGroup = [];
 }
-
-// build pagination links from number of groups.
-function buildPagination(){
-	if (liGroups.length>1){
-		$(".pagination").empty();
-		liGroups.forEach(function(value, i){
-			$(".pagination").append('<li id='+(i+1)+'><a>'+(i+1)+'</a></li>');
-		});
-	}
-}
-
-// display based on selected link.
-function display(){
-	liGroups[selected].forEach(function(value){
-		value.style.display = 'block';
-	});
-}
-
-// listen for changes in selected link and update display.
-function paginatnListener(){
-	$('.pagination li').click(function(event){
+const watchPagination=()=>{
+	$('.pagination li').on('click', function(event){
 		$('.student-item').hide();
-		selected = $(this).attr('id')-1;
-		display();
+		let selected = $(this).find('a').text();
+		showSelected(selected);
 	});
 }
-
-groupListItems(allStudents);
-buildPagination();
-var selected = 0; // initial selected pagination link is the first group.
-display();
-
-paginatnListener();
-
-// listen for student search
-$('.student-search button').click(function(){
-	$('.student-item').hide();
-	var search = $('.student-search input').val();
-	// clear error messages if any.
-	$('#error').remove();
-	// clear pagination links in case no results found.
+const buildPagination=()=>{
 	$(".pagination").empty();
+	let students = $('.visible');
 
-	// if search cleared display all students.
-	if (search === ''){
-		groupListItems(allStudents);
-		buildPagination();
-		selected = 0; 
-		display();
-		paginatnListener();
-
-	// else filter all students by name and email matches.
-	}else{
-		var matches = allStudents.filter(function(student){
-			var name = student.getElementsByTagName('h3')[0].textContent;
-			var email = student.getElementsByClassName("email")[0].textContent;
-			return name.indexOf(search)>=0||email.indexOf(search)>=0;
+	if (students.length>10){
+		students.each(function(i, element){
+			if((i+1)%10==0){
+				let text = ((i+1)/10);
+				$(".pagination").append('<li><a>'+text+'</a></li>');
+			}
 		});
-
-		// if no matches found hide all student list items and show error msg.
-		if (matches.length==0){
-			$('.student-item').hide();
-			$('.student-list').append("<p id='error'>No students found.</p>");
-
-		// else display matches in groups of 10 students and build pagination accordingly. 
-		// also listen for clicks on pagination links.
-		}else{
-			groupListItems(matches);
-			buildPagination();
-			selected = 0; 
-			display();
-			paginatnListener();
-		}		
+		if(students.length%10!==0){
+			let text = $(".pagination li").length + 1;
+			$(".pagination").append('<li><a>'+text+'</a></li>');
+		}
+		watchPagination();
+		showSelected(1);
 	}
-});
+
+}
+const noStudentsFound=()=>{
+	$('.student-item').hide().removeClass('visible');
+	$('.student-list').append("<p id='error'>No students found.</p>");
+	$(".pagination").empty();
+}
+const nameOrEmailSearch=(search)=>{
+	return $('.student-item').filter(function(student){
+			var name = $(this).find('h3').text();
+			var email = $(this).find(".email").text();
+			return name.indexOf(search)>=0||email.indexOf(search)>=0;
+	});
+}
+const watchForm=()=>{
+	$('.student-search').on('submit', function(e){
+		e.preventDefault();
+		$('.student-item').hide().removeClass('visible');
+		$('#error').remove();
+
+		let searchTerm = $('.student-search input').val();
+
+		if (searchTerm === ''){
+			$('.student-item').show().addClass('visible');
+			buildPagination();
+		}else{
+			let matches = nameOrEmailSearch(searchTerm);
+			if(matches.length===0){
+				noStudentsFound();
+			}else{
+				matches.show().addClass('visible');
+				buildPagination();
+			}
+
+		}
+	});
+}
+const addForm=()=>{
+	$(".page-header").append(`
+		<form class='student-search'>
+			<input type="text" name="search">
+			<button type="submit">Search</button>
+		</form>
+	`);
+}
+
+const initialDisplay=()=>{
+	$(".page").append("<div class='pagination'></div>");
+	addForm();
+	watchForm();
+	$('.student-item').addClass('visible');
+	buildPagination();
+}
+
+$(initialDisplay);
---
+++ js/sandbox.js
@@ -0,0 +1,103 @@
+$(".page").append("<div class='pagination'></div>");
+$('.student-item').hide();
+$(".page-header").append(`
+	<div class='student-search'>
+		<input type="text" name="search">
+		<button>Search</button>
+	</div>
+`);
+
+var allStudents = [];
+$('li').each(function(i, element){
+	allStudents.push(element);
+});
+
+// a group of list items that will display on each "page".
+var liGroup = [];
+// an arr of groups.
+var liGroups = [];
+
+function groupListItems(listItems){
+	liGroups = [];
+	listItems.forEach(function(student, i){
+		// construct groups.
+		liGroup.push(student);
+		if((i+1)%10==0){
+			//  construct liGroups arr and clear group arr for next grouping.
+			liGroups.push(liGroup);
+			liGroup = [];
+		}
+	});
+	// contruct last group out of the left-over list items.
+	liGroups.push(liGroup);
+	liGroup = [];
+}
+
+// build pagination links from number of groups.
+function buildPagination(){
+	if (liGroups.length>1){
+		$(".pagination").empty();
+		liGroups.forEach(function(value, i){
+			$(".pagination").append('<li id='+(i+1)+'><a>'+(i+1)+'</a></li>');
+		});
+	}
+}
+
+// display based on selected link.
+function display(){
+	liGroups[selected].forEach(function(value){
+		value.style.display = 'block';
+	});
+}
+
+// listen for changes in selected link and update display.
+
+
+groupListItems(allStudents);
+buildPagination();
+var selected = 0; // initial selected pagination link is the first group.
+display();
+
+paginatnListener();
+
+// listen for student search
+$('.student-search button').click(function(){
+	$('.student-item').hide();
+	var search = $('.student-search input').val();
+	// clear error messages if any.
+	$('#error').remove();
+	// clear pagination links in case no results found.
+	$(".pagination").empty();
+
+	// if search cleared display all students.
+	if (search === ''){
+		groupListItems(allStudents);
+		buildPagination();
+		selected = 0; 
+		display();
+		paginatnListener();
+
+	// else filter all students by name and email matches.
+	}else{
+		var matches = allStudents.filter(function(student){
+			var name = student.getElementsByTagName('h3')[0].textContent;
+			var email = student.getElementsByClassName("email")[0].textContent;
+			return name.indexOf(search)>=0||email.indexOf(search)>=0;
+		});
+
+		// if no matches found hide all student list items and show error msg.
+		if (matches.length==0){
+			$('.student-item').hide();
+			$('.student-list').append("<p id='error'>No students found.</p>");
+
+		// else display matches in groups of 10 students and build pagination accordingly. 
+		// also listen for clicks on pagination links.
+		}else{
+			groupListItems(matches);
+			buildPagination();
+			selected = 0; 
+			display();
+			paginatnListener();
+		}		
+	}
+});
